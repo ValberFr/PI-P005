f9ce62ae12982666e1c4016f517397d9bd0e4f94
joaopedropinto <jpcic07@gmail.com>
2023-09-18 16:06:40 -0300

    Finalizando atividade pratica 5

---
+++ Joao/4_Operacao_Bitbit.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int info, genes = 0, numGene = 0;
+
+    cout << "Digite a informação genética da planta (um número entre 0 e 255): ";
+    cin >> info;
+
+    if (info < 0 || info > 255){
+      cout << "Valor incorreto!" << endl;
+      return 1;
+    }
+
+    unsigned char infoChar = (unsigned char) info;
+
+    for (int i = 0; i < 8; i++) {
+        if (infoChar & (1 << i)) {
+            genes++;
+        }
+    }
+
+    cout << "A planta possui " << genes << " genes."<< endl;
+
+    cout << "Digite o número do gene que você deseja verificar (1 a 8): ";
+    cin >> numGene;
+
+    if (numGene >= 1 && numGene <= 8) {
+        if (infoChar & (1 << (numGene - 1))) {
+            cout << "O gene " << numGene << " está presente nesta planta." << endl;
+        } else {
+            cout << "O gene " << numGene << " não está presente nesta planta." << endl;
+        }
+    } else {
+        cout << "Número de gene inválido. Digite um valor entre 1 e 8." << endl;
+    }
+
+    return 0;
+}
